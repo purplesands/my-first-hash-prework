d663ebd6fbe9a5e2e47cf3a88a137ec01851e740
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 02:31:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,5 +35,7 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
     shipping_manifest["muskets"] = 2
     shipping_manifest["gun powder"] = 4
+    shipping_manifest["oil paintings"] = 3
+
   return shipping_manifest
 end
